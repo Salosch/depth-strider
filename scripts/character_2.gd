36f4c20261fd56de8b9c65f8b142ef9f6794821d
Himayat Kamran <himayatkamran@hotmail.de>
2024-06-25 00:56:21 +0200

    Add second character

---
+++ scripts/character_2.gd
@@ -0,0 +1,85 @@
+extends CharacterBody2D
+
+const SPEED = 200
+const REPAIR_TIME = 2.0
+
+var total_elapsed_repair_time = 0.0
+var current_repairing_node: Node = null
+
+@onready var animation_player = $Sprite2D/AnimationPlayer
+@onready var audio_player = $AudioStreamPlayer2D
+@onready var ship_node = get_tree().get_root().get_node("Game/CharacterCanvas/Ship")
+@onready var repair_progress = get_tree().get_root().get_node("Game/UI/Control/RepairProgress")
+
+func _ready():
+	var sb = StyleBoxFlat.new()
+	repair_progress.add_theme_stylebox_override("fill", sb)
+	sb.bg_color = Color('582080')
+
+func _physics_process(_delta):
+	velocity = Input.get_vector("ui_left_2", "ui_right_2", "ui_up_2", "ui_down_2") * SPEED
+
+	if Input.is_action_pressed("ui_right_2"):
+		animation_player.play("walk_right")
+	elif Input.is_action_pressed("ui_left_2"):
+		animation_player.play("walk_left")
+	elif Input.is_action_pressed("ui_up_2"):
+		animation_player.play("walk_up")
+	elif Input.is_action_pressed("ui_down_2"):
+		animation_player.play("walk_down")
+	else:
+		animation_player.play("idle")
+		
+	move_and_slide()
+
+func _on_damage_hitbox_area_entered(node_to_repair: Node, delta) -> void:
+	handle_repair(node_to_repair, delta, "ui_accept_2")
+
+func _on_breach_hitbox_area_entered(node_to_repair: Node, delta) -> void:
+	handle_repair(node_to_repair, delta, "ui_select_2")
+
+func handle_repair(node: Node, delta: float, action: String) -> void:
+	if Input.is_action_pressed(action):
+		play_sound_off_action(action)
+		animation_player.play("repair")
+		total_elapsed_repair_time += delta
+		self.set_physics_process(false)
+		repair_progress.visible = true
+		repair_progress.value = total_elapsed_repair_time
+
+	if Input.is_action_just_released(action):
+		stop_sound()
+		animation_player.play("idle")
+		total_elapsed_repair_time = 0
+		self.set_physics_process(true)
+		repair_progress.visible = false
+		repair_progress.value = 0
+
+	if total_elapsed_repair_time > REPAIR_TIME:
+		stop_sound()
+		animation_player.play("idle")
+		total_elapsed_repair_time = 0
+		self.set_physics_process(true)
+		repair_progress.visible = false
+
+		if action == "ui_accept_2":
+			node.get_parent().queue_free()
+		elif action == "ui_select_2":
+			ship_node.respawn_damage(node)
+			node.get_parent().queue_free()
+
+func play_sound_off_action(action: String) -> void:
+	if action == "ui_accept_2":
+		if not audio_player.playing:
+			var hammering = load("res://assets/sound_effects/hammering.wav")
+			audio_player.stream = hammering
+			audio_player.play()
+
+	elif action == "ui_select_2":
+		if not audio_player.playing:
+			var welding = load("res://assets/sound_effects/welding.wav")
+			audio_player.stream = welding
+			audio_player.play()
+
+func stop_sound() -> void:
+	audio_player.stop()
