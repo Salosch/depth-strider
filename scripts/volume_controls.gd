d55c562e0a35ba7db5b1441591e41c567d478883
CanerHSMA <caneraliji020402@gmail.com>
2024-06-30 15:03:39 +0200

    fix: style change volume control

--- scripts/volume_controls.gd
+++ scripts/volume_controls.gd
@@ -9,6 +9,7 @@ func _ready():
 	value_changed.connect(_on_value_changed)
 	
 	value = db_to_linear(AudioServer.get_bus_volume_db(bus_index))
+	
 
 func _on_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_db(bus_index, linear_to_db(value))
